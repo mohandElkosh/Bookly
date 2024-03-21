996261efe42f64146d7ce70bb73e5491e2402bc0
mohandElkosh <mokelkosh@gmail.com>
2024-03-21 04:42:23 +0200

    create custom book details app bar

--- lib/Core/utils/app_router.dart
+++ lib/Core/utils/app_router.dart
@@ -1,9 +1,12 @@
+import 'package:ecommerce_get/Features/home/presentation/views/book_detail_view.dart';
 import 'package:ecommerce_get/Features/home/presentation/views/home.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../Features/presentation/views/splash_view.dart';
 
 abstract class AppRouter {
+  static const KhomeView = '/homeView';
+  static const KBookDetailsView = '/BookDetailsView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -13,6 +16,10 @@ abstract class AppRouter {
       GoRoute(
         path: '/homeview',
         builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: KBookDetailsView,
+        builder: (context, state) => const BookDetailsView(),
       ),
     ],
   );
--- lib/Features/home/presentation/views/views_Model/best_seller_listview_item.dart
+++ lib/Features/home/presentation/views/views_Model/best_seller_listview_item.dart
@@ -1,6 +1,8 @@
+import 'package:ecommerce_get/Core/utils/app_router.dart';
 import 'package:ecommerce_get/Core/utils/style.dart';
 import 'package:ecommerce_get/Features/home/presentation/views/views_Model/book_rating.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../../constants.dart';
 
@@ -9,64 +11,70 @@ class BestSellerListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.red,
-                image: const DecorationImage(
-                  image: AssetImage(KtestImage),
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.KBookDetailsView);
+      },
+      child: SizedBox(
+        height: 125,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.5 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.red,
+                  image: const DecorationImage(
+                    image: AssetImage(KtestImage),
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(
-            width: 30,
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: Text(
-                    'Hary Poter and the Goblet of Fire',
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    style: Styles.textStyle20.copyWith(
-                        fontFamily: KGtSectraFine, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                const SizedBox(
-                  height: 3,
-                ),
-                const Text(
-                  'J.K. Rowling',
-                  style: Styles.textStyle14,
-                ),
-                const SizedBox(
-                  height: 3,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      '19.99 \$',
-                      style: Styles.textStyle20
-                          .copyWith(fontWeight: FontWeight.bold),
-                    ),
-                    const Spacer(),
-                    const BookRating(),
-                  ],
-                ),
-              ],
+            const SizedBox(
+              width: 30,
             ),
-          ),
-        ],
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    child: Text(
+                      'Hary Poter and the Goblet of Fire',
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: Styles.textStyle20.copyWith(
+                          fontFamily: KGtSectraFine,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  const Text(
+                    'J.K. Rowling',
+                    style: Styles.textStyle14,
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '19.99 \$',
+                        style: Styles.textStyle20
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      const Spacer(),
+                      const BookRating(),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
