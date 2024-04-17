e536eb820d46acfc23c2abaa6685bbcf2be75c57
mohandElkosh <111207275+mohandElkosh@users.noreply.github.com>
2024-04-17 04:46:41 +0200

    add rating to book details

--- lib/Features/home/presentation/views/views_Model/book_detail_view_body.dart
+++ lib/Features/home/presentation/views/views_Model/book_detail_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_get/Core/utils/style.dart';
+import 'package:ecommerce_get/Features/home/presentation/views/views_Model/book_rating.dart';
 import 'package:ecommerce_get/Features/home/presentation/views/views_Model/custom_book_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -35,6 +36,12 @@ class BookDetailsViewBody extends StatelessWidget {
               style: Styles.textStyle18.copyWith(
                   fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
             ),
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          const BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
           ),
         ],
       ),
--- lib/Features/home/presentation/views/views_Model/book_rating.dart
+++ lib/Features/home/presentation/views/views_Model/book_rating.dart
@@ -4,14 +4,18 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../../../../Core/utils/style.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
+  const BookRating(
+      {super.key, this.mainAxisAlignment = MainAxisAlignment.start});
 
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         const Icon(
           FontAwesomeIcons.solidStar,
+          size: 18,
           color: Color(0xffFFDD4f),
         ),
         const SizedBox(
