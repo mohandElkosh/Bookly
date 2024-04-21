0c94fdf84ebf002f9adfd92454cb7bfe7ec40e0f
mohandElkosh <111207275+mohandElkosh@users.noreply.github.com>
2024-04-21 14:48:35 +0200

    create custom search text field

--- lib/Features/home/presentation/views/views_Model/custom_app_bar.dart
+++ lib/Features/home/presentation/views/views_Model/custom_app_bar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../../../Core/utils/app_router.dart';
 import '../../../../../constants.dart';
 
 class CustomAppBar extends StatelessWidget {
@@ -21,7 +23,9 @@ class CustomAppBar extends StatelessWidget {
             ),
             const Spacer(),
             IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  GoRouter.of(context).push(AppRouter.KsearchView);
+                },
                 icon: const Icon(
                   FontAwesomeIcons.magnifyingGlass,
                   color: Colors.white,
