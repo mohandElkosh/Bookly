21b3827f705a0ce092f482f881b64d198c011538
mohandElkosh <111207275+mohandElkosh@users.noreply.github.com>
2024-04-26 20:45:11 +0300

    implement fetch newest books (get reqwest)

--- lib/Features/home/data/reopo/home_repo.dart
+++ lib/Features/home/data/reopo/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:ecommerce_get/Core/errors/failures.dart';
 import 'package:ecommerce_get/Features/home/data/models/book_model/book_model.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failuer, List<BookModel>>> fetchBestSellerBoks();
+  Future<Either<Failuer, List<BookModel>>> fetchNewestBooks();
   Future<Either<Failuer, List<BookModel>>> fetchFeaturedBooks();
 }
--- lib/Features/home/data/reopo/home_repo_implementation.dart
+++ lib/Features/home/data/reopo/home_repo_implementation.dart
@@ -1,13 +1,28 @@
 import 'package:dartz/dartz.dart';
 import 'package:ecommerce_get/Core/errors/failures.dart';
+import 'package:ecommerce_get/Core/utils/api_services.dart';
 import 'package:ecommerce_get/Features/home/data/models/book_model/book_model.dart';
 import 'package:ecommerce_get/Features/home/data/reopo/home_repo.dart';
 
 class HomeRepoImpl implements HomeRepo {
   @override
-  Future<Either<Failuer, List<BookModel>>> fetchBestSellerBoks() {
-    // TODO: implement fetchBestSellerBoks
-    throw UnimplementedError();
+  final ApiServices apiServices;
+
+  HomeRepoImpl({required this.apiServices});
+  @override
+  Future<Either<Failuer, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiServices.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &g=subject:Programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
